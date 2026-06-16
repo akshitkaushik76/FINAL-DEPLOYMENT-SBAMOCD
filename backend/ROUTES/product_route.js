1e174884ba19fec5d9b630b8c8d5c98542f840f7
akshitkaushik76 <akshitkaushik307@gmail.com>
2026-06-16 23:13:16 +0530

    fixed router

--- backend/ROUTES/product_route.js
+++ backend/ROUTES/product_route.js
@@ -10,8 +10,12 @@ router.route('/get-products/:branchid').get(
     auth_controller.protect_owner,
     product_controller.get_products
 );
-router.route('/update-quantity/:branch_id/:business_code').patch(auth_controller.protect_owner,product_controller.updatequantity);
-router.route('/delete-product/:business_code/:branch_id').delete(auth_controller.protect_owner,product_controller.delete_product_information);
+// Change these two routes
+router.route('/update-quantity/:branch_id/:business_code')
+    .patch(auth_controller.protect_owner, product_controller.updatequantity);
+
+router.route('/delete-product/:business_code/:branch_id')
+    .delete(auth_controller.protect_owner, product_controller.delete_product_information);
 router.route('/add-measurable-product/:business_code/:branchid/').post(auth_controller.protect_owner,product_controller.add_measured_product);
 
 module.exports = router;
